0eaed2534cb50d0a5d0767ec5beda244affe7433
Vanessa Dreux <vanessa.dreux@gmail.com>
2021-03-19 14:48:21 +0100

    Create Update Lead Status (light version)

---
+++ UpdateStatusController_Light.js
@@ -0,0 +1,16 @@
+({
+ onload: function(component, event, helper){
+    },
+    handleClick: function(component, event, helper){
+    component.set("v.leadRecord.Status", "Working - Contacted");
+       component.find("leadRec").saveRecord($A.getCallback(function(saveResult) {
+           if (saveResult.state === "SUCCESS") {
+        } else {
+            component.set("v.recordSaveError",'Unknown problem, state: ' + saveResult.state + ', error: ' + 
+              JSON.stringify(saveResult.error));
+        }
+    }));
+        
+    $A.get("e.force:closeQuickAction").fire();  
+    },
+})
