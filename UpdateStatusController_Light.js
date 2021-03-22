da76f1452ec071d30b392e92c9bed0d48f77b780
Vanessa Dreux <vanessa.dreux@gmail.com>
2021-03-22 10:14:31 +0100

    Update status to Contacted

--- UpdateStatusController_Light.js
+++ UpdateStatusController_Light.js
@@ -2,7 +2,7 @@
  onload: function(component, event, helper){
     },
     handleClick: function(component, event, helper){
-    component.set("v.leadRecord.Status", "Working - Contacted");
+    component.set("v.leadRecord.Status", "Contacted");
        component.find("leadRec").saveRecord($A.getCallback(function(saveResult) {
            if (saveResult.state === "SUCCESS") {
         } else {
